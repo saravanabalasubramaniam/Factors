ca33001af3e2398473505645f04af436d8a04e85
saravana <saravanasaru14313@gmail.com>
2019-02-21 06:43:41 -0800

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class Factors
+{
+  public static void main(String args[])
+  {
+	Scanner input=new Scanner(System.in);
+	int a=input.nextInt();
+	for(int i=1;i<=a;i++)
+	{
+		if(a%i==0)
+		{
+			System.out.println(i);
+		}
+	}
+  }
+}
